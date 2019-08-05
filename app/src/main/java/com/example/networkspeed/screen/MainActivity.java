91e6dca7050fa0b148f679792532e11590a30cc7
Yummy <ujjang@trinitysoft.co.kr>
2019-08-05 21:23:04 +0900

    fixed_2

--- app/src/main/java/com/example/networkspeed/screen/MainActivity.java
+++ app/src/main/java/com/example/networkspeed/screen/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.networkspeed.screen;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -9,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.*;
 import android.view.View;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.networkspeed.R;
 
@@ -18,6 +21,7 @@ import java.util.List;
 public class MainActivity extends Activity implements OnItemSelectedListener {
     private Spinner spinner;
     ArrayList<String> paths = new ArrayList<String>();
+    ArrayList<String> execApp = new ArrayList<String>();
     PackageManager pm;
 
     @Override
@@ -28,9 +32,16 @@ public class MainActivity extends Activity implements OnItemSelectedListener {
 
         pm = getPackageManager();
         List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        paths.add("Application Lists");
+        execApp.add("");
 
         for (ApplicationInfo packageInfo : packages) {
-            paths.add(packageInfo.packageName);
+            String tmp;
+            tmp = (String) (packageInfo != null ? pm.getApplicationLabel(packageInfo) : "Unknown");
+            if(!tmp.startsWith("com")) {
+                paths.add(tmp);
+                execApp.add(packageInfo.packageName);
+            }
         }
 
         spinner = (Spinner)findViewById(R.id.platform_list);
@@ -44,13 +55,26 @@ public class MainActivity extends Activity implements OnItemSelectedListener {
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-        Intent startApp = pm.getLaunchIntentForPackage(paths.get(position));
-        if(startApp != null)
-            MainActivity.this.startActivity(startApp);
+        Intent startApp = pm.getLaunchIntentForPackage(execApp.get(position));
+        if(startApp != null) {
+            try {
+                startActivity(startApp);
+            } catch (ActivityNotFoundException err) {
+                Toast t = Toast.makeText(getApplicationContext(),
+                        "Application not found", Toast.LENGTH_SHORT);
+                t.show();
+            }
+        }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
         // TODO Auto-generated method stub
+    }
+
+    public static String getApplicationName(Context context) {
+        ApplicationInfo applicationInfo = context.getApplicationInfo();
+        int stringId = applicationInfo.labelRes;
+        return stringId == 0 ? applicationInfo.nonLocalizedLabel.toString() : context.getString(stringId);
     }
 }
